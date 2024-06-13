5c2d28848929c22ecb5f0ee225a209df07d16f95
KeimaSenpai <hg4052650@gmail.com>
2024-06-13 19:46:38 -0400

    Ya salen los valores de la configuración del usuario

--- views/user.py
+++ views/user.py
@@ -1,19 +1,33 @@
 import os
 import flet as ft
 import minecraft_launcher_lib as mll
+import json
 
 from minecraft_launcher.minecraft import latest_versions, save_config
 
 user_windows = os.environ["USERNAME"]
 minecraft_directory = f"C://Users//{user_windows}//AppData//Roaming//.xlauncher"
 
+# Ruta del archivo de configuración
+ruta_json = f"{minecraft_directory}//configuration.json"
 
-#Obtiene la lista de versiones instaladas
+# Verificar si el archivo de configuración existe y cargar datos si existe
+if os.path.exists(ruta_json):
+    with open(ruta_json, 'r') as file:
+        config_data = json.load(file)
+else:
+    config_data = {
+        "mine_user": "",
+        "ram": "",
+        "version": ""
+    }
+
+# Obtiene la lista de versiones instaladas
 versions = latest_versions()
 opciones = [ft.dropdown.Option(version) for version in versions]
 
-
 title = ft.Text('Perfil', font_family='mine_dun', size=30)
+
 username = ft.TextField(
     hint_text="User Name",
     width=250,
@@ -22,11 +36,13 @@ username = ft.TextField(
     border=ft.InputBorder.NONE,
     border_radius=30,
     cursor_height=20,
-    text_style= ft.TextStyle(
+    text_style=ft.TextStyle(
         font_family='mine',
         size=15,
     ),
+    value=config_data.get("mine_user", "")
 )
+
 ram = ft.TextField(
     hint_text="RAM",
     width=250,
@@ -35,23 +51,36 @@ ram = ft.TextField(
     border=ft.InputBorder.NONE,
     border_radius=30,
     cursor_height=20,
-    text_style= ft.TextStyle(
+    text_style=ft.TextStyle(
         font_family='mine',
         size=15,
     ),
+    value=config_data.get("ram", "")
 )
+
 version = ft.Dropdown(
     width=250,
     height=50,
     bgcolor="#343434",
     focused_bgcolor="#343434",
     border=ft.InputBorder.NONE,
-    text_style= ft.TextStyle(
+    text_style=ft.TextStyle(
         font_family='mine',
         size=15,
     ),
     options=opciones,
+    value=config_data.get("version", "")
 )
+
+def save_configuration(e):
+    config = {
+        "mine_user": username.value,
+        "ram": ram.value,
+        "version": version.value
+    }
+    with open(ruta_json, 'w') as file:
+        json.dump(config, file)
+
 guardar_btn = ft.ElevatedButton(
     "Guardar",
     style=ft.ButtonStyle(
@@ -62,9 +91,8 @@ guardar_btn = ft.ElevatedButton(
         shadow_color="#178c4a",
         elevation=5,
     ),
-    on_click=lambda _: save_config(mine_user=username.value, ram=ram.value, version=version.value),
+    on_click=save_configuration,
 )
-
 
 user_page = ft.Stack(
     [
@@ -74,7 +102,7 @@ user_page = ft.Stack(
                     title,
                     username,
                     ram,
-                    version, 
+                    version,
                     guardar_btn,
                 ],
                 alignment=ft.MainAxisAlignment.CENTER,
